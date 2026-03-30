fb873db940d52e7fb4ca54f7c4e39e64a6644339
kiabetsu <srcback@yandex.ru>
2026-03-30 23:32:27 +0300

    add singup logic

--- frontend/src/features/Auth/AuthForm/components/AuthForm.tsx
+++ frontend/src/features/Auth/AuthForm/components/AuthForm.tsx
@@ -3,33 +3,48 @@ import { Input } from './Input/Input';
 import { useAuthForm } from '../hooks/useAuthForm';
 
 export const AuthForm = () => {
-  const { register, handleSubmit, errors } = useAuthForm();
+  const {
+    loginRegister,
+    loginHandleSubmit,
+    loginErrors,
+    isLogin,
+    onSwitchForm,
+    singupErrors,
+    singupHandleSubmit,
+    singupRegister,
+  } = useAuthForm();
 
   return (
-    <div className={styles.contentBox}>
-      <div className={styles.carousel}>
-        <form onSubmit={handleSubmit} className={styles.inputBox}>
-          <h1>Sign in</h1>
+    <>
+      <div className={styles.contentBox}>
+        <div className={styles.carousel}>
+          <form onSubmit={loginHandleSubmit} className={styles.inputBox}>
+            <h1>Sign in</h1>
 
-          <Input label="Username" error={errors.username} {...register('username')} autoFocus />
-          <Input
-            label="Password"
-            type="password"
-            error={errors.password}
-            {...register('password')}
-          />
+            <Input
+              label="Username"
+              error={loginErrors.username}
+              {...loginRegister('username')}
+              autoFocus
+            />
+            <Input
+              label="Password"
+              type="password"
+              error={loginErrors.password}
+              {...loginRegister('password')}
+            />
 
-          <div className={styles.loginButtonAndAgree}>
-            <label className={styles.rememberCheckbox}>
-              <input type="checkbox" name="rememberMe" id="" />
-              <span className={styles.checkmark}></span>
-              <span>Remember me</span>
-            </label>
-            <button type="submit" className={styles.loginButton}>
-              Log in
-              {/* {status === 'padding' && <div className={styles.spinner}></div>} */}
-            </button>
-            {/* <span>
+            <div className={styles.loginButtonAndAgree}>
+              <label className={styles.rememberCheckbox}>
+                <input type="checkbox" name="rememberMe" id="" />
+                <span className={styles.checkmark}></span>
+                <span>Remember me</span>
+              </label>
+              <button type="submit" className={styles.loginButton}>
+                Log in
+                {/* {status === 'padding' && <div className={styles.spinner}></div>} */}
+              </button>
+              {/* <span>
               By creating an account, you agree to the &nbsp;
               <a> Terms of use </a>
               &nbsp; and &nbsp;
@@ -43,10 +58,10 @@ export const AuthForm = () => {
                 <a>Forget your password</a>
               </div>
             </div> */}
-          </div>
-        </form>
+            </div>
+          </form>
 
-        {/* <form className={styles.inputBox}>
+          {/* <form className={styles.inputBox}>
           <h1>Sign up</h1>
 
           <div className={styles.loginButtonAndAgree}>
@@ -61,7 +76,13 @@ export const AuthForm = () => {
             </span>
           </div>
         </form> */}
+        </div>
       </div>
-    </div>
+      <div className={styles.switchButtonContainer}>
+        <button className={styles.switchButton} onClick={() => onSwitchForm()}>
+          switch
+        </button>
+      </div>
+    </>
   );
 };
--- frontend/src/features/Auth/AuthForm/hooks/useAuthForm.ts
+++ frontend/src/features/Auth/AuthForm/hooks/useAuthForm.ts
@@ -1,27 +1,69 @@
+import React from 'react';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 export const useAuthForm = () => {
-  const schema = z.object({
+  const [isLogin, setIsLogin] = React.useState(false);
+
+  const loginSchema = z.object({
     username: z.string().min(3),
     password: z.string().min(6),
-    email: z.email(),
   });
 
-  type RegisterFormData = z.infer<typeof schema>;
+  type LoginFormData = z.infer<typeof loginSchema>;
 
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<RegisterFormData>({
-    resolver: zodResolver(schema),
+    register: loginRegister,
+    handleSubmit: loginHandleSubmit,
+    formState: { errors: loginErrors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
   });
 
-  const onSubmit = () => {
-    console.log('submit');
+  const onSubmitLogin = () => {
+    console.log('submit login');
   };
 
-  return { register, handleSubmit: handleSubmit(onSubmit), errors };
+  const onSwitchForm = () => {
+    setIsLogin(!isLogin);
+    console.log(`isLogin now ${isLogin}`);
+  };
+
+  const singupSchema = z
+    .object({
+      username: z.string().min(3),
+      email: z.email(),
+      password: z.string().min(6),
+      confirm: z.string(),
+    })
+    .refine((data) => data.password === data.confirm, {
+      message: "Passwords don't match",
+      path: ['confirm'],
+    });
+
+  type SingupFormData = z.infer<typeof singupSchema>;
+
+  const {
+    register: singupRegister,
+    handleSubmit: singupHandleSubmit,
+    formState: { errors: singupErrors },
+  } = useForm<SingupFormData>({
+    resolver: zodResolver(singupSchema),
+  });
+
+  const onSubmitSingup = () => {
+    console.log('submit singup');
+  };
+
+  return {
+    loginRegister,
+    loginHandleSubmit: loginHandleSubmit(onSubmitLogin),
+    loginErrors,
+    isLogin,
+    onSwitchForm,
+    singupRegister,
+    singupHandleSubmit: singupHandleSubmit(onSubmitSingup),
+    singupErrors,
+  };
 };
